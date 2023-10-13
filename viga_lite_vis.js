4f41899b8b361ccb583968a80d2c9288077b560c
ashley0416 <akok0009@student.monash.edu>
2023-10-13 10:27:49 +0800

    6th commit

--- viga_lite_vis.js
+++ viga_lite_vis.js
@@ -13,3 +13,11 @@ vegaEmbed("#bubble_plot", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     
 }).catch(console.error);
+
+var vg_3 = "vis3.vl.json"
+
+vegaEmbed("#bar_chart", vg_3).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    
+}).catch(console.error);
