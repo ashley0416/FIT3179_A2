2e1137fcef5b5dfdea3a65271ebb9d00f9e36df7
ashley0416 <akok0009@student.monash.edu>
2023-10-17 13:20:51 +0800

    14th commit

--- viga_lite_vis.js
+++ viga_lite_vis.js
@@ -21,3 +21,11 @@ vegaEmbed("#bar_chart", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     
 }).catch(console.error);
+
+var vg_4 = "vis4.vl.json"
+
+vegaEmbed("#slope_chart", vg_4).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    
+}).catch(console.error);
